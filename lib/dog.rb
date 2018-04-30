4a66ad4a9132aa7834a1c276e57eb4ec7c0405c0
Melissa Real <melissa.real@hotmail.com>
2018-04-30 13:55:54 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,17 +1,22 @@
 class Dog
- 
+
   @@all = []
- 
+
   attr_accessor :name
- 
+
   def initialize(name)
     @name = name
     @@all << self
   end
- 
+
   def self.all
     @@all.each do |dog|
       puts dog.name
     end
   end
+
+  def clear_all
+
+  end 
+  
 end
